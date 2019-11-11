75e248a785563344e71564c6e30f1b8113448503
Matheus Abreu <matheussla@users.noreply.github.com>
2019-11-10 23:22:20 -0300

    password hash

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.post('/register', async (req, res) =>{
     try {
+        if(await User.findOne({ email }))
+            return res.status(400).send({error : 'User already exists'});
+
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({ user });
     } catch (err) {
